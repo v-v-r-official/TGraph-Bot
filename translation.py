fb3cf51ce69f85c643df02bd984a28eb52956125
TG Expert <78008481+v-v-r-official@users.noreply.github.com>
2021-04-04 20:09:12 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -11,7 +11,7 @@ class Translation(object):
 /help - <b>How To Use Me</b>
 /about - <b>More About Me</b>"""
     ABOUT_TEXT = """**➥ My Name** : ```Telegraph Bot```
-**➥ Creator** : <a href=https://t.me/D_ar_k_Angel>Dark Angel</a>
+**➥ Creator** : <a href=https://t.me/VKPROJECTS>VK PROJECTS</a>
 **➥ Credits** : ```Everyone in this journey```
 **➥ Language** : ```Python3```
 **➥ Library** : <a href=https://docs.pyrogram.org>Pyrogram asyncio 0.18.0</a>
